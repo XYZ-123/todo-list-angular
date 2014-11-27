dcf944558a6bbdd63c47073f86200addb7b55327
Arthur <rudyuk_artur@ukr.net>
2014-11-27 19:00:17 +0200

    added icon, stylings and some behaviour

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,9 @@ var todoAppControllers = angular.module('todoAppControllers', ['ngStorage']);
 todoAppControllers.controller("todoListCtrl",['$scope','$localStorage',function($scope,$localStorage)
 {
 		$localStorage.todos = [];
-		$localStorage.todos[1] = {id:1,title:"Hello I am first",importance:1, status:0};
-		$localStorage.todos[2] = {id:2,title:"Hello I am second",importance:2, status:0.3};
-		
+		$localStorage.todos[0] = {id:0,title:"Hello I am first",importance:1, status:0, click:"editTodo(0)"};
+		$localStorage.todos[1] = {id:1,title:"Hello I am second",importance:2, status:0.3, click:"editTodo(1)"};
+
 		$scope.todos = $localStorage.todos;
 		$scope.addTodo = function(todo)
 		{
@@ -16,5 +16,13 @@ todoAppControllers.controller("todoListCtrl",['$scope','$localStorage',function(
 		$scope.removeTodo = function(todo)
 		{
 			$localStorage.todos.splice(todo.id, 1);
+		}
+		$scope.getProp = function(prop, obj)
+		{
+			return $scope.$eval(prop, obj);
+		}
+		$scope.editTodo = function(id)
+		{
+			$localStorage.todos[id].title = "Edited!";
 		}
 }]);
