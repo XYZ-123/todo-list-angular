91302eee6fc01e79cb59246c5619c75ba5673879
XYZ-123 <rudyuk_artur@ukr.net>
2014-11-30 20:17:07 +0200

    Added styling for todo list, added details html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,17 +3,17 @@ var todoAppControllers = angular.module('todoAppControllers', ['ngStorage']);
 
 todoAppControllers.controller("todoListCtrl",['$scope','$localStorage',function($scope,$localStorage)
 {
-		$localStorage.todos = [];
-		$localStorage.todos[0] = {id:0,inEditingMode:false,isEditing: "isEditing(0)", title:"Hello I am first",importance:1, status:0, edit:"editTodo(0)", remove:"removeTodo(0)"};
-		$localStorage.todos[1] = {id:1,inEditingMode:false,isEditing: "isEditing(1)", title:"Hello I am second",importance:2, status:0.6, edit:"editTodo(1)", remove:"removeTodo(1)"};
-		$localStorage.todos[2] = {id:2,inEditingMode:false,isEditing: "isEditing(2)", title:"Hello I am second",importance:3, status:1, edit:"editTodo(2)", remove:"removeTodo(2)"};
+		//$localStorage.todos = [];
+		//$localStorage.todos[0] = {id:0,inEditingMode:false,isEditing: "isEditing(0)", title:"Hello I am first",importance:1, status:0, edit:"editTodo(0)", remove:"removeTodo(0)"};
+		//$localStorage.todos[1] = {id:1,inEditingMode:false,isEditing: "isEditing(1)", title:"Hello I am second",importance:2, status:0.6, edit:"editTodo(1)", remove:"removeTodo(1)"};
+		//$localStorage.todos[2] = {id:2,inEditingMode:false,isEditing: "isEditing(2)", title:"Hello I am second",importance:3, status:1, edit:"editTodo(2)", remove:"removeTodo(2)"};
 
 		$scope.todos = $localStorage.todos;
 		
 		$scope.addTodo = function(todoTitle)
 		{
 			var nextId = $scope.todos.length;
-			$scope.todos[nextId] = {id:nextId,inEditingMode:false,isEditing: "isEditing("+nextId+")",title:todoTitle, importance: 0, status:0,edit:"editTodo("+nextId+")", remove:"removeTodo("+nextId+")"};
+			$scope.todos[nextId] = {id:nextId,inEditingMode:false,isEditing: "isEditing("+nextId+")",title:todoTitle, importance: 1, status:0,edit:"editTodo("+nextId+")", remove:"removeTodo("+nextId+")"};
 			$scope.todoTitle = "";
 		}
 		
@@ -46,3 +46,9 @@ todoAppControllers.controller("todoListCtrl",['$scope','$localStorage',function(
 			$scope.todos[id].inEditingMode = !$scope.todos[id].inEditingMode ;
 		}
 }]);
+
+todoAppControllers.controller("todoDetailsCtrl",['$scope','$localStorage','$routeParams',function($scope, $localStorage, $routeParams)
+{
+	 $scope.todoId = $routeParams.todoId;
+}]);
+
