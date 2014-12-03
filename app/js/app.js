1fbe03620d062522c77e4015a4dea0ebdc783f84
Arthur <rudyuk_artur@ukr.net>
2014-12-03 18:14:35 +0200

    Added possibility to go to different paths

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,7 @@ todoApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .when("/todo",{templateUrl:"partials/todo-list.html",controller:"todoListCtrl"})
   .when("/todo/:todoId",{templateUrl:"partials/todo-details.html",controller:"todoDetailsCtrl"})
-  .when("/todo/active",{templateUrl:"partials/todo-details.html",controller:"todoDetailsCtrl"})
+  .when("/todo/active",{templateUrl:"partials/todo-details.html",controller:"todoActiveDetailsCtrl"})
   .when("/todo/completed",{templateUrl:"partials/todo-list.html",controller:"todoListCtrl"})
   .otherwise({redirectTo: '/todo'});
 }]);
